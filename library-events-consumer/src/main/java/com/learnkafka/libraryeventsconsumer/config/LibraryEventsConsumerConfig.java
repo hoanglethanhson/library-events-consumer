316467222f8fd0cc83778721790f3519996179af
leson2912 <leson1998ls@gmail.com>
2023-02-09 00:21:42 +0700

    first commit

--- library-events-consumer/src/main/java/com/learnkafka/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
+++ library-events-consumer/src/main/java/com/learnkafka/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
@@ -1,10 +1,28 @@
 package com.learnkafka.libraryeventsconsumer.config;
 
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
 
 @Configuration
 @EnableKafka
 public class LibraryEventsConsumerConfig {
+    @Bean
+    @ConditionalOnMissingBean(name = "kafkaListenerContainerFactory")
+    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
+            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+            ObjectProvider<ConsumerFactory<Object, Object>> kafkaConsumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        configurer.configure(factory, (ConsumerFactory<Object, Object>) kafkaConsumerFactory);
+        factory.setConcurrency(3);
+        //factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
 
+        return factory;
+    }
 }
